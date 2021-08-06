df13c1d24798c21409d7c3b7558119d678b4ddcb
ahmedmahmoudsoliman <mitomahmoud2022@gmail.com>
2021-08-06 20:42:25 +0200

    dasboard install

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use  App\Http\Controllers\Frontend\FrontendController;
+use App\Http\Controllers\Backend\BackendController;
+
+Route::get('/', [FrontendController::class,'index'])->name('frontend.index');
+Route::get('/cart', [FrontendController::class,'cart'])->name('frontend.cart');
+Route::get('/checkout', [FrontendController::class,'checkout'])->name('frontend.checkout');
+Route::get('/detail', [FrontendController::class,'detail'])->name('frontend.detail');
+Route::get('/shop', [FrontendController::class,'shop'])->name('frontend.shop');
+
+
+Route::get('/admin/login',[BackendController::class,'login'])->name('backend.login');
+Route::get('/admin/forgot-password',[BackendController::class,'forgot_password'])->name('backend.forgot_password');
+Route::get('/admin/index',[BackendController::class,'index'])->name('backend.index');
 
 Auth::routes();
 
